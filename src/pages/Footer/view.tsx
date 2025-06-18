e8004db5bbba6eef1c9cd9fca173f780bf5a306c
buatong01 <133838800+buatong01@users.noreply.github.com>
2025-06-18 23:51:58 +0700

    Update FooterView component: add social media icons, improve layout, and enhance copyright notice

--- src/pages/Footer/view.tsx
+++ src/pages/Footer/view.tsx
@@ -1,15 +1,19 @@
+import facebook from "../../assets/facebook.png";
+import tiktok from "../../assets/tiktok.png";
+import instragram from "../../assets/instagram.png";
+
 function FooterView() {
   return (
     <div className="bg-white w-screen ">
       <div className="border-y border-gray-900 h-auto px-4 py-4 pt-4 pb-12 ">
         <div className="flex gap-2.5">
-          <div className="text-white font-bold bg-black px-2 lg:text-4xl md:text-3xl sm:text-2xl">
+          <div className="text-white font-bold bg-black px-2 lg:text-2xl md:text-xl ">
             B
           </div>
-          <div className="text-white font-bold bg-black px-2 lg:text-4xl md:text-3xl sm:text-2xl">
+          <div className="text-white font-bold bg-black px-2 lg:text-2xl md:text-xl ">
             B
           </div>
-          <div className="text-white font-bold bg-black px-2 lg:text-4xl md:text-3xl sm:text-2xl">
+          <div className="text-white font-bold bg-black px-2 lg:text-2xl md:text-xl ">
             C
           </div>
         </div>
@@ -28,7 +32,34 @@ function FooterView() {
 
         <div className="border-y-[0.5px] border-gray-200 mb-4"></div>
 
-        <div className="text-black font-semibold text-base">Follow BBC on:</div>
+        <div className="flex flex-col sm:flex-row gap-2.5 items-start sm:items-center">
+          <div className="text-black font-semibold text-base">
+            Follow BBC on:
+          </div>
+          <div className="flex gap-2.5">
+            <img src={facebook} alt="Facebook" className="h-6 w-auto " />
+            <img src={instragram} alt="Instagram" className="h-6 w-auto " />
+            <img src={tiktok} alt="TikTok" className="h-6 w-auto " />
+          </div>
+        </div>
+
+        <div className="flex-wrap flex gap-x-6 mt-3.5 gap-y-3">
+          <p className="text-[12px] text-black">Terms of Use</p>
+          <p className="text-[12px] text-black">About the BBC</p>
+          <p className="text-[12px] text-black">Private Policy</p>
+          <p className="text-[12px] text-black">Cookies</p>
+          <p className="text-[12px] text-black">Accesibility Help</p>
+          <p className="text-[12px] text-black">Contact the BBC</p>
+          <p className="text-[12px] text-black">Advertise with us</p>
+          <p className="text-[12px] text-black">Do not share or sell my info</p>
+          <p className="text-[12px] text-black">Contact technical support</p>
+        </div>
+
+        <p className="text-[12px] text-black mt-5">
+          Copyright 2025 BBC. All rights reserved. The
+          <em> BBC is not responsible for the content of external sites. </em>
+          <strong>Read about our approach to external linking.</strong>
+        </p>
       </div>
     </div>
   );
