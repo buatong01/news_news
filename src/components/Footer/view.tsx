2436baf13abcf80cba202a130241fa59d1ae81a0
buatong01 <133838800+buatong01@users.noreply.github.com>
2025-06-19 22:38:16 +0700

    Add new assets and enhance FooterView with language selection feature
    - Added down and up icons for language toggle in FooterView
    - Implemented language selection functionality with state management
    - Updated package.json and package-lock.json to include react-router-dom and @tanstack/react-query
    - Created new DetailPage view component

--- src/components/Footer/view.tsx
+++ src/components/Footer/view.tsx
@@ -1,8 +1,13 @@
 import facebook from "../../assets/facebook.png";
 import tiktok from "../../assets/tiktok.png";
 import instragram from "../../assets/instagram.png";
+import down from "../../assets/down.png";
+import up from "../../assets/up.png";
+import { useState } from "react";
 
 function FooterView() {
+  const [isLanguagesOpen, setIsLanguagesOpen] = useState(false);
+
   return (
     <div className="bg-white w-screen">
       <div className="border-t border-gray-900 h-auto px-4 py-4 pt-4 pb-12 ">
@@ -30,11 +35,55 @@ function FooterView() {
           </div>
         </div>
 
-        <button className="h-[44px] w-[204px] bg-gray-200 flex justify-center items-center mt-6.5 mb-4.5">
+        <button
+          onClick={() => setIsLanguagesOpen(!isLanguagesOpen)}
+          className={`h-[44px] w-[204px] bg-gray-200 flex justify-center items-center mt-6.5 ${
+            !isLanguagesOpen && "mb-4.5"
+          }`}
+        >
           <div className="text-black text-sm font-semibold">
             BBC in other languages
           </div>
+          {isLanguagesOpen ? (
+            <img src={down} alt="down" className="ml-1.5 h-3 w-3" />
+          ) : (
+            <img src={up} alt="up" className="ml-1.5 h-3 w-3" />
+          )}
         </button>
+
+        {isLanguagesOpen && (
+          <div className="bg-gray-200 w-full px-12 py-12 mb-4.5">
+            <div className="flex flex-col items-center mb-5 ">
+              <h2 className="text-black font-bold text-base sm:text-xl mb-4">
+                The BBC is in multiple languages
+              </h2>
+              <h3 className="text-black text-sm sm:text-base">
+                Read the BBC In your own language
+              </h3>
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 space-x-16 space-y-4  sm:w-max sm:mx-auto">
+              <p className="text-black text-[12px] underline cursor-pointer">
+                Oduu Afaan Oromootiin
+              </p>
+              <p className="text-black text-[12px]  underline cursor-pointer">
+                Gujarati ગુજરાતી સમાચાર
+              </p>
+              <p className="text-black text-[12px] underline cursor-pointer">
+                Pashto پښتو
+              </p>
+              <p className="text-black  text-[12px] underline cursor-pointer">
+                Telugu తెలుగు వార్తలు
+              </p>
+              <p className="text-black text-[12px] underline cursor-pointer">
+                Amharic ዜና በአማርኛ
+              </p>
+              <p className="text-black text-[12px] underline cursor-pointer">
+                Igbo AKỤKỌ N’IGBO
+              </p>
+            </div>
+          </div>
+        )}
 
         <div className="border-y-[0.5px] border-gray-200 mb-4"></div>
 
