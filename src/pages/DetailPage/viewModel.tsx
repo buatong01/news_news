e9a21f7061d0bc870a590f5d928d6a4e49831dac
buatong01 <133838800+buatong01@users.noreply.github.com>
2025-06-26 23:14:30 +0700

    Refactor useDetailViewModel: streamline article retrieval logic by consolidating category checks into a single function; improve readability and maintainability.

--- src/pages/DetailPage/viewModel.tsx
+++ src/pages/DetailPage/viewModel.tsx
@@ -11,48 +11,18 @@ function useDetailViewModel() {
   const { searchArticles } = useSearchContext();
 
   // ไม่มี key id ให้ search ต้อง search index
-  const article = useMemo(() => {
-    if (!articles || !index) return undefined;
-
-    if (category === "head") {
-      return articles[Number(index)];
-    } else if (category === "search") {
-      return searchArticles[Number(index)];
-    } else if (category === "mustread") {
-      const result = everythingArticles.sort(
-        (a: Article, b: Article) =>
-          new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
-      );
-      return result[Number(index)];
-    } else {
-      const result = everythingArticles
-        .filter(
-          (article: Article) => article.category?.toLowerCase() === category
-        )
-        .sort(
-          (a: Article, b: Article) =>
-            new Date(b.publishedAt).getTime() -
-            new Date(a.publishedAt).getTime()
-        );
-      return result[Number(index)];
-    }
-  }, [articles, everythingArticles, category, index]);
-
-  const articless = useMemo(() => {
-    if (!articles || !index) return undefined;
-
+  const getArticlesByCategory = () => {
     if (category === "head") {
       return articles;
     } else if (category === "search") {
       return searchArticles;
     } else if (category === "mustread") {
-      const result = everythingArticles.sort(
+      return [...everythingArticles].sort(
         (a: Article, b: Article) =>
           new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
       );
-      return result;
     } else {
-      const result = everythingArticles
+      return everythingArticles
         .filter(
           (article: Article) => article.category?.toLowerCase() === category
         )
@@ -61,9 +31,20 @@ function useDetailViewModel() {
             new Date(b.publishedAt).getTime() -
             new Date(a.publishedAt).getTime()
         );
-      return result;
     }
-  }, [articles, everythingArticles, category, index]);
+  };
+
+  const articless = useMemo(getArticlesByCategory, [
+    articles,
+    everythingArticles,
+    searchArticles,
+    category,
+  ]);
+
+  const article = useMemo(() => {
+    if (!articless || !index) return undefined;
+    return articless[Number(index)];
+  }, [articless, index]);
 
   return {
     article,
